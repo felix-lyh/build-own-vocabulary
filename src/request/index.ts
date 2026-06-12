241a96cc586ee9bc091490a5ad91ed1b44e24595
lyh <>
2026-06-12 15:55:18 +0800

    no message

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,5 +1,6 @@
+'use client';
 import axios from 'axios';
-const baseURL = process.env.API_BASE_URL || 'http://localhost:3000';
+const baseURL:string = process.env.NEXT_PUBLIC_API_BASE_URL!;
 
 const instance = axios.create({
     baseURL,
